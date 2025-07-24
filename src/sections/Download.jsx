0ab783c9a73d60b00e397713e2aedf4abf88e9cb
Aftab <uetianaftab@gmail.com>
2025-07-24 06:55:52 -0700

    add mobile section in download

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -18,54 +18,119 @@ const browsers = [
     { name: "Arc", icon: "https://framerusercontent.com/images/nFhDQ3PlcARouqwLahKLRoWY.svg" },
 ];
 
+const mobile = [
+    { name: "Andriod", icon: "https://framerusercontent.com/images/AA9C0on2lL61VGndTN3egni3YqY.svg" },
+    { name: "iOS", icon: "https://framerusercontent.com/images/6guVArhs20n1oqrc2yN1Xlh8UA.svg" },
+];
+
+
+
 const Download = () => {
     return (
 
-        <Box className="w-full flex justify-center px-4 py-20 ">
-            <Box boxShadow={3} className="flex flex-col w-full max-w-7xl rounded-3xl shadow-[0px_4px_184px_#a9abb522] p-8 md:p-16 relative bg-[#7369ff] bg-opacity-30 backdrop-blur-xl">
-                {/* Top Title */}
-                <Typography
-                    variant="h2"
-                    className="text-center font-bold text-white"
+        <Box>
+            <Box className=" hidden md:flex w-full items-center justify-center px-4 py-20   ">
+                <Box
                     sx={{
-                        fontFamily: "SF Pro Rounded, -apple-system, BlinkMacSystemFont, sans-serif",
-                        fontWeight: 800,
-                        fontSize: { xs: "28px", md: "44px", lg: "56px" },
-                        lineHeight: { xs: "36px", md: "54px", lg: "66px" },
-                        mb: 8,
+                        background: "radial-gradient(100% 100% at 50% 100%,#a1cbfd,#e0e5ff 32.64734820868138%,#f1f3f7)"
                     }}
-                >
-                    Choose Your Browser
-                </Typography>
+                    className="flex flex-col w-full max-w-7xl rounded-3xl shadow-[0px_4px_184px_#a9abb522] p-8 md:p-16 relative bg-opacity-30 backdrop-blur-xl">
+                    <Box className="flex flex-col-reverse md:flex-row items-center gap-12">
 
-                {/* Content Section */}
-                <Box className="flex flex-col-reverse md:flex-row items-center gap-12">
-                    {/* Cards Grid */}
-                    <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 flex-1">
-                        {browsers.map((browser) => (
-                            <div
-                                key={browser.name}
-                                className="p-6 rounded-2xl bg-white bg-opacity-60 shadow-lg backdrop-blur-md flex flex-col items-center transition-transform transform hover:-translate-y-2 hover:shadow-2xl border border-gray-100"
+                        <Stack className='flex-1'>
+                            <Typography
+                                variant="h2"
+                                className="text-center font-bold text-[#171717]"
+                                sx={{
+                                    fontFamily: "SF Pro Rounded, -apple-system, BlinkMacSystemFont, sans-serif",
+                                    fontWeight: 800,
+                                    fontSize: { xs: "28px", md: "44px", lg: "56px" },
+                                    lineHeight: { xs: "36px", md: "54px", lg: "66px" },
+                                    mb: 8,
+                                }}
                             >
-                                <img src={browser.icon} alt="" />
-                                <h3 className="mt-4 font-semibold text-gray-800">{browser.name}</h3>
+                                Download for
+                                <br />
+                                Desktop
+                            </Typography>
+                            <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 flex-1">
+                                {browsers.map((browser) => (
+                                    <div
+                                        key={browser.name}
+                                        className="p-6 rounded-2xl bg-white bg-opacity-60 shadow-lg backdrop-blur-md flex flex-col items-center transition-transform transform hover:-translate-y-2 hover:shadow-2xl border border-gray-100"
+                                    >
+                                        <img src={browser.icon} alt="" />
+                                        <h3 className="mt-4 font-semibold text-gray-800">{browser.name}</h3>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </Stack>
 
-                    {/* Right Side Image */}
-                    <Box
-                        component="img"
-                        src="https://framerusercontent.com/images/VMlyJdWdfaRDDAbGARnULWDMEVc.png?scale-down-to=1024"
-                        alt="Download Devices"
-                        sx={{
-                            maxWidth: "100%",
-                            width: { xs: "240px", md: "300px", lg: "400px" },
-                            objectFit: "contain",
-                        }}
-                    />
+                        <Box
+                            component="img"
+                            src="https://framerusercontent.com/images/VMlyJdWdfaRDDAbGARnULWDMEVc.png?scale-down-to=1024"
+                            alt="Download Devices"
+                            sx={{
+                                maxWidth: "100%",
+                                width: { xs: "240px", md: "300px", lg: "400px" },
+                                objectFit: "contain",
+                            }}
+                        />
+                    </Box>
                 </Box>
             </Box>
+
+            <Box className="w-full flex items-center justify-center px-4 py-20 ">
+                <Box sx={{
+                    background: "radial-gradient(100% 100% at 50% 100%,#a1cbfd,#e0e5ff 32.64734820868138%,#f1f3f7)"
+                }} className="flex flex-col w-full max-w-7xl rounded-3xl shadow-[0px_4px_184px_#a9abb522] p-8 md:p-16 relative  bg-opacity-30 backdrop-blur-xl">
+                    <Box className="flex flex-col-reverse md:flex-row items-center gap-12">
+
+                        <Box>
+
+                            <Box
+                                component="img"
+                                src="https://framerusercontent.com/images/ojR9drUQuF1ZfjCgBAEzJnQE.png?scale-down-to=1024"
+                                alt="Download Devices"
+                                sx={{
+                                    maxWidth: "100%",
+                                    width: { xs: "240px", md: "300px", lg: "400px" },
+                                    objectFit: "contain",
+                                }}
+                            />
+                        </Box>
+                        <Stack className='flex-1'>
+                            <Typography
+                                variant="h2"
+                                className="text-center font-bold text-[#171717]"
+                                sx={{
+                                    fontFamily: "SF Pro Rounded, -apple-system, BlinkMacSystemFont, sans-serif",
+                                    fontWeight: 800,
+                                    fontSize: { xs: "28px", md: "44px", lg: "56px" },
+                                    lineHeight: { xs: "36px", md: "54px", lg: "66px" },
+                                    mb: 8,
+                                }}
+                            >
+                                Download for
+                                <br />
+                                Mobile
+                            </Typography>
+                            <div className="flex justify-center items-center  gap-6 flex-1">
+                                {mobile.map((browser) => (
+                                    <div
+                                        key={browser.name}
+                                        className="px-10 py-6 rounded-4xl text-center bg-white bg-opacity-60 shadow-lg backdrop-blur-md flex-col items-center transition-transform transform hover:-translate-y-2 hover:shadow-2xl border border-gray-100"
+                                    >
+                                        <img src={browser.icon} alt="" className='w-[80px]' />
+                                        <h3 className="mt-4 font-semibold text-gray-800">{browser.name}</h3>
+                                    </div>
+                                ))}
+                            </div>
+                        </Stack>
+                    </Box>
+                </Box>
+            </Box>
+
         </Box>
     );
 };
