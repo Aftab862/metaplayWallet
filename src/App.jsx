329414053c9da61a69c33b54530e26b81a824760
Aftab Ahmad <uetianaftab@gmail.com>
2025-08-20 00:46:21 -0700

    implemnet footer icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
-import { DivWrapper } from "./sections/DivWrapper";
+
+import DivWrapper from "./sections/DivWrapper";
 import Hero from "./sections/hero";
-import { FrameWrapper } from "./sections/FrameWrapper";
-import { OverlapGroupWrapper } from "./sections/OverlapGroupWrapper";
-import MotionFadeIn from "./components/MotionFadeIn";
 import ZeroPersonal from "./sections/ZeroPersonal";
 import Marquee from "./sections/Marque";
 
@@ -11,29 +8,56 @@ import Marquee from "./sections/Marque";
 const App = () => {
   return (
     <div>
-      {/* <MotionFadeIn delay={0}> */}
       <Hero />
-      {/* </MotionFadeIn> */}
-
-      {/* <MotionFadeIn delay={0}> */}
       <ZeroPersonal />
-      {/* </MotionFadeIn> */}
-      {/* <MotionFadeIn delay={0}> */}
       <Marquee />
-      {/* </MotionFadeIn> */}
-      {/* <MotionFadeIn delay={0}> */}
       <DivWrapper />
-      {/* </MotionFadeIn> */}
     </div>
   );
 };
 
 export default App;
-{/* <FrameWrapper /> */ }
 
-//  <MotionFadeIn delay={0}>
-//       <OverlapGroupWrapper />
-//     </MotionFadeIn>
-{
-  /* <OverlapWrapper /> */
-}
+
+
+
+
+// import React, { useEffect } from "react";
+// import { DivWrapper } from "./sections/DivWrapper";
+// import Hero from "./sections/hero";
+// import { FrameWrapper } from "./sections/FrameWrapper";
+// import { OverlapGroupWrapper } from "./sections/OverlapGroupWrapper";
+// import MotionFadeIn from "./components/MotionFadeIn";
+// import ZeroPersonal from "./sections/ZeroPersonal";
+// import Marquee from "./sections/Marque";
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <MotionFadeIn delay={0}> */}
+//       <Hero />
+//       {/* </MotionFadeIn> */}
+
+//       {/* <MotionFadeIn delay={0}> */}
+//       <ZeroPersonal />
+//       {/* </MotionFadeIn> */}
+//       {/* <MotionFadeIn delay={0}> */}
+//       <Marquee />
+//       {/* </MotionFadeIn> */}
+//       {/* <MotionFadeIn delay={0}> */}
+//       <DivWrapper />
+//       {/* </MotionFadeIn> */}
+//     </div>
+//   );
+// };
+
+// export default App;
+// {/* <FrameWrapper /> */ }
+
+// //  <MotionFadeIn delay={0}>
+// //       <OverlapGroupWrapper />
+// //     </MotionFadeIn>
+// {
+//   /* <OverlapWrapper /> */
+// }
